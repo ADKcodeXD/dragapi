31d84574336c51f61eaa1deaadc97f1084ef220b
ADKcode <790532173@qq.com>
2022-08-31 00:24:32 +0800

    更新示例

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="home">
     <ul class="container">
-      <li
-        v-for="item in data"
-        :key="item.code"
-        class="box-item"
-        draggable
-        :ref="`li-${item.code}`"
-      >
+      <li v-for="item in data" :key="item.code" class="box-item" draggable :ref="`li-${item.code}`"
+        :class="{ 'over-border': item.isHover }">
         <div>
           <p>13456</p>
           <span>shaouifhao</span>
@@ -22,62 +17,68 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 interface FileItem {
   name: string;
   code: number;
+  isHover: boolean;
 }
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
+@Component
 export default class HomeView extends Vue {
   data: FileItem[] = [
-    { name: "abc", code: 1 },
-    { name: "sahou", code: 2 },
-    { name: "gfabguoh", code: 3 },
-    { name: "asdxxz", code: 4 },
-    { name: "asxccv", code: 5 },
-    { name: "sfaxxxz", code: 6 },
+    { name: "abc", code: 1, isHover: false },
+    { name: "sahou", code: 2, isHover: false },
+    { name: "gfabguoh", code: 3, isHover: false },
+    { name: "asdxxz", code: 4, isHover: false },
+    { name: "asxccv", code: 5, isHover: false },
+    { name: "sfaxxxz", code: 6, isHover: false },
   ];
-
-  handleDragStart(ev: DragEvent) {
-    console.log(ev);
-    console.log("start");
+  private lastEnterNode!: Element;
+  private currentElment!: FileItem | null;
+  handleDragStart(ev: any, item: FileItem) {
     if (ev.dataTransfer) {
       // Add the target element's id to the data transfer object
       ev.dataTransfer.dropEffect = "move";
+      this.currentElment = item
+      console.log(this.currentElment);
     }
   }
   handleDragEnd(ev: DragEvent) {
-    console.log(ev);
-    console.log("end");
     if (ev.dataTransfer) {
       // Add the target element's id to the data transfer object
       ev.dataTransfer.dropEffect = "move";
+      this.currentElment = null
     }
   }
-  handleDragOver(ev: DragEvent) {
-    console.log(ev);
-    console.log("over");
-    let targetEl = ev.target as HTMLElement;
+  handleDragEnter(ev: any, item: FileItem) {
+    this.lastEnterNode = ev.target
     if (ev.dataTransfer) {
-      // Add the target element's id to the data transfer object
-      ev.dataTransfer.dropEffect = "move";
+      if (this.currentElment && this.currentElment.code !== item.code) {
+        item.isHover = true
+        let index = this.data.findIndex(el => el.code === item.code)
+        this.data.splice(index, 1, item)
+      }
+      ev.dataTransfer.dropEffect = "copy";
+    }
+  }
+  handleDragLeave(ev: any, item: FileItem) {
+    if (this.lastEnterNode === ev.target) {
+      if (ev.dataTransfer) {
+        let currentNode = ev.dataTransfer.getData('file')
+        item.isHover = false
+        let index = this.data.findIndex(el => el.code === item.code)
+        this.data.splice(index, 1, item)
+        ev.dataTransfer.dropEffect = "copy";
+      }
     }
   }
   mounted() {
-    console.log("iam");
     if (this.$refs) {
-      console.log("yes");
       for (let item of this.data) {
         console.log(this.$refs[`li-${item.code}`]);
         let el = this.$refs[`li-${item.code}`][0] as Element;
-        el.addEventListener("dragstart", (event as DragEvent) =>
-          this.handleDragStart(event,item)
-        );
-        el.addEventListener("dragover", this.handleDragOver);
+        el.addEventListener("dragstart", (e) => this.handleDragStart(e, item));
+        el.addEventListener("dragenter", (e) => this.handleDragEnter(e, item));
+        el.addEventListener("dragleave", (e) => this.handleDragLeave(e, item));
         el.addEventListener("dragend", this.handleDragEnd);
       }
     }
